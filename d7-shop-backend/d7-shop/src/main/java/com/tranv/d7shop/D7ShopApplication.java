c3663280d9a4fc9e8376a36ea55e3042ea4f43e3
NGUYENTRAPC\nguye <nguyenvantra2444@gmail.com>
2024-04-19 16:36:02 +0700

    create new Project

---
+++ d7-shop-backend/d7-shop/src/main/java/com/tranv/d7shop/D7ShopApplication.java
@@ -0,0 +1,13 @@
+package com.tranv.d7shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D7ShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D7ShopApplication.class, args);
+	}
+
+}
---
+++ d7-shop-backend/d7-shop/src/test/java/com/tranv/d7shop/D7ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tranv.d7shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D7ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
